9ccdc6e103e614640a2dd1389a85cc5186dc52b7
Shrivaniselva <81732154+Shrivaniselva@users.noreply.github.com>
2022-04-28 21:49:20 -0400

    Create 180. Consecutive Numbers.sql

---
+++ Medium/180. Consecutive Numbers.sql
@@ -0,0 +1,56 @@
+/* 180. Consecutive Numbers
+Table: Logs
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| num         | varchar |
++-------------+---------+
+id is the primary key for this table.
+id is an autoincrement column.
+ 
+
+Write an SQL query to find all numbers that appear at least three times consecutively.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Logs table:
++----+-----+
+| id | num |
++----+-----+
+| 1  | 1   |
+| 2  | 1   |
+| 3  | 1   |
+| 4  | 2   |
+| 5  | 1   |
+| 6  | 2   |
+| 7  | 2   |
++----+-----+
+Output: 
++-----------------+
+| ConsecutiveNums |
++-----------------+
+| 1               |
++-----------------+
+Explanation: 1 is the only number that appears consecutively for at least three times.
+*/
+-- Solution: MS SQL Server
+
+;WITH   CTE AS
+(
+SELECT  id, num ,row_number() OVER (PARTITION BY num ORDER BY id) rn
+FROM    Logs
+)
+
+SELECT  DISTINCT num AS ConsecutiveNums
+FROM    CTE
+GROUP BY num, (id-rn)
+HAVING COUNT((id-rn)) >= 3
